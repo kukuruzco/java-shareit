a8358411237074d83f2f5e4d2b15372d716fc27a
clonefuture <clonefuture@gmail.com>
2026-02-11 23:05:44 +0200

    Add controllers

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.item;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ItemMapper {
+
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                item.getRequestId()
+        );
+    }
+
+    public static Item toItem(ItemDto itemDto, Long ownerId) {
+        Item item = new Item();
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setOwnerId(ownerId);
+        item.setRequestId(itemDto.getRequestId());
+        return item;
+    }
+}
