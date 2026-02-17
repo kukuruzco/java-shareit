e06782d75a9ef830e83a3cc147142f4273a81b28
kukuruzco <aleksandr.kruasanoff@yandex.ru>
2026-02-17 16:28:28 +0200

    Add queries to create tables in schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(520) NOT NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id),
+    CONSTRAINT UQ_USER_EMAIL UNIQUE (email)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(2000) NOT NULL,
+    available BOOLEAN NOT NULL,
+    owner_id BIGINT,
+    request_id BIGINT,
+    CONSTRAINT pk_item PRIMARY KEY (id),
+    CONSTRAINT fk_items_to_users FOREIGN KEY (owner_id) REFERENCES users(id)
+);
